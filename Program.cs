ac1d90f4ebe97136d2ecd8210cd10fc04e73b591
Robyn Orellana Santiago <robynorellana18@outlook.com>
2022-02-25 22:50:53 -0600

    Version com monedas 1.02

--- Program.cs
+++ Program.cs
@@ -1,68 +1,68 @@
 ﻿static void desglose()
 {
-    float can, can1, can10, can20, can50, can100, can5, cm50, cm25, cm5, cm10;
-    can = can1 = can10 = can20 = can50 = can100 = can5 = cm50= cm25= cm5= cm10= 0.00f;
+    float re, can, can1, can10, can20, can50, can100, can5, cm50, cm25, cm5, cm10;
+    re = can = can1 = can10 = can20 = can50 = can100 = can5 = cm50= cm25= cm5= cm10= 0.00f;
    
     Console.WriteLine("Ingrese cantidad");
     can = float.Parse(Console.ReadLine());
 
-    if ((can>=100))
+    if (can>=100)
     {
         can100 = (int) (can / 100);
-       can = can - (can100 * 100);
+        can = can - (can100 * 100);
 
     }
-    if ((can >= 50))
+    if (can >= 50)
     {
         can50 = (int)(can / 50);
         can = can - (can50 * 50);
 
     }
-    if ((can>=20))
+    if (can>=20)
     {
         can20= (int)(can/ 20); 
         can=can - (can20 * 20);
 
     }
-    if ((can>=10))
+    if (can>=10)
     {
         can10 = (int)(can / 10);
         can = can - (can10 * 10);
     }
-    if ((can>=5))
+    if (can>=5)
     {
         can5 = (int)(can / 5);
         can = can - (can5 * 5);
     }
-    if ((can >=1))
+    if (can >=1)
     {
         can1 = (int)(can / 1);
         can = can - (can1 * 1);
     }
-    if ((can>=0.50))
+    if (can>=0.50)
     {
         cm50 = (int)(can / 0.50);
-        can = can - (cm50 * 00.50f);
+        can = can - (cm50 * 0.50f);
     }
-    if ((can >= 0.25))
+    if (can >= 0.25)
     {
         cm25 = (int)(can / 0.25);
-        can = can - (cm25 * 00.25f);
+        can = can - (cm25 * 0.25f);
     }
-    if ((can >= 0.10))
+    if (can >= 0.10)
     {
         cm10 = (int)(can / 0.10);
-        can = can - (cm10 * 00.10f);
+        can = can - (cm10 * 0.10f);
     }
-    if ((can >= 0.05))
+    if (can >= 0.05)
     {
         cm5 = (int)(can / 0.05);
-        can = can - (cm5 * 00.05f);
+        can = can - (cm5 * 0.05f);
     }
-    if ((can >= 0.01f))
+    if (can >= 00.01f)
     {
-        can = (int)(can / 00.01);
-        can = (int)can - (can * 00.01f);
+        can = (int)(can / 00.01f);  
+        can=(int)Math.Round(can);   
         
     }
 
@@ -76,7 +76,7 @@
     Console.WriteLine($"Monedas de 0.25={cm25}");
     Console.WriteLine($"Monedas de 0.10={cm10}");
     Console.WriteLine($"Monedas de 0.05={cm5}");
-    Console.WriteLine($"Monedas de 0.01={can}");
+    Console.WriteLine($"Monedas de 0.01={re}");
 }
 
 desglose();
