62170c14dcbcc514a70b079788340c300784c9d8
Robyn Orellana Santiago <robynorellana18@outlook.com>
2022-02-25 18:24:05 -0600

    version con monedas 1.0 iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿static void desglose()
 {
-    float can, can10, can20, can50, can100, can5;
-    can = can10 = can20 = can50 = can100 = can5 = 0.00f;
-    string linea;
+    float can, can1, can10, can20, can50, can100, can5, cm50, cm25, cm5, cm10;
+    can = can1 = can10 = can20 = can50 = can100 = can5 = cm50= cm25= cm5= cm10= 0.00f;
+   
     Console.WriteLine("Ingrese cantidad");
     can = float.Parse(Console.ReadLine());
 
@@ -36,7 +36,33 @@
     }
     if ((can >=1))
     {
-        can = (int)(can / 1);
+        can1 = (int)(can / 1);
+        can = can - (can1 * 1);
+    }
+    if ((can>=0.50))
+    {
+        cm50 = (int)(can / 0.50);
+        can = can - (cm50 * 00.50f);
+    }
+    if ((can >= 0.25))
+    {
+        cm25 = (int)(can / 0.25);
+        can = can - (cm25 * 00.25f);
+    }
+    if ((can >= 0.10))
+    {
+        cm10 = (int)(can / 0.10);
+        can = can - (cm10 * 00.10f);
+    }
+    if ((can >= 0.05))
+    {
+        cm5 = (int)(can / 0.05);
+        can = can - (cm5 * 00.05f);
+    }
+    if ((can >= 0.01f))
+    {
+        can = (int)(can / 00.01);
+        can = (int)can - (can * 00.01f);
         
     }
 
@@ -45,8 +71,12 @@
     Console.WriteLine($"Billetes de 20={can20}");
     Console.WriteLine($"Billetes de 10={can10}");
     Console.WriteLine($"Billetes de 5={can5}");
-    Console.WriteLine($"Billetes de 1={can}");
-
+    Console.WriteLine($"Billetes de 1={can1}");
+    Console.WriteLine($"Monedas de 0.50={cm50}");
+    Console.WriteLine($"Monedas de 0.25={cm25}");
+    Console.WriteLine($"Monedas de 0.10={cm10}");
+    Console.WriteLine($"Monedas de 0.05={cm5}");
+    Console.WriteLine($"Monedas de 0.01={can}");
 }
 
 desglose();
